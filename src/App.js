271523e16be95781ba8d62b01c6c23e1e0e45b8b
unknown <saradawsone@gmail.com>
2024-05-15 04:00:55 -0600

    nav styled

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import { Link } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
-      <header style={{backgroundImage: 'url(/images/TopHeaderImage.png)', backgroundSize: '100vw', backgroundRepeat:'no-repeat', backgroundColor: '#103c00', height:'100vh'}}>
-        <nav>
-          <Link to='/'>APPOINTMENTS</Link>
-          <Link to='/'>MEDICAL CARD INFO</Link>
-          <Link to='/'>FAQs</Link>
-          <Link to='/'>PHARMACIES</Link>
-          <Link to='/'>ABOUT</Link>
+      <header style={{backgroundImage: 'url(/images/TopHeaderImage.png)', backgroundSize: '100vw', backgroundRepeat:'no-repeat', backgroundColor: '#103c00', height:'100vh', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'space-between'
+      }}>
+        <nav style={{marginTop: '4rem', width: '80%', display: 'flex', justifyContent: 'space-around'}}>
+          <Link className='nav-link' to='/'>APPOINTMENTS</Link>
+          <Link className='nav-link' to='/'>MEDICAL CARD INFO</Link>
+          <Link className='nav-link' to='/'>FAQs</Link>
+          <Link className='nav-link' to='/'>PHARMACIES</Link>
+          <Link className='nav-link' to='/'>ABOUT</Link>
         </nav>
         <img src='/images/updatedAssets/UtahGrownLogoWhiteBorder.png' alt='Utah Grown Logo White Boarder' style={{ textAlign: 'center'}}/>
       </header>
