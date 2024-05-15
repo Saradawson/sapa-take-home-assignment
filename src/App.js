02b95487bfb7c97b3a1b1f9d29e4df309193b7f7
unknown <saradawsone@gmail.com>
2024-05-15 03:17:24 -0600

    header images and nav added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import { Link } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header style={{backgroundImage: 'url(/images/TopHeaderImage.png)', backgroundSize: '100vw', backgroundRepeat:'no-repeat', backgroundColor: '#103c00', height:'100vh'}}>
+        <nav>
+          <Link to='/'>APPOINTMENTS</Link>
+          <Link to='/'>MEDICAL CARD INFO</Link>
+          <Link to='/'>FAQs</Link>
+          <Link to='/'>PHARMACIES</Link>
+          <Link to='/'>ABOUT</Link>
+        </nav>
+        <img src='/images/updatedAssets/UtahGrownLogoWhiteBorder.png' alt='Utah Grown Logo White Boarder' style={{ textAlign: 'center'}}/>
       </header>
+      <main>
+
+      </main>
+      <footer></footer>
     </div>
   );
 }
